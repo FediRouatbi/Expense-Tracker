005b6bd0fd7865dcabc607ea0441b500e9c4d368
FediRouatbi <fedirouatbi@gmail.com>
2022-05-25 13:18:13 +0100

    'delete items'

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -12,12 +12,7 @@ const Expenses = () => {
         <div className="expenses__title--column">amount</div>
         <div className="expenses__title--column">date</div>
       </div>
-      <div className="expenses__items">
-        <Item />
-        <Item />
-        <Item />
-        <Item />
-      </div>
+      <div className="expenses__items"></div>
     </div>
   );
 };
