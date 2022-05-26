86a7435d69a4dadc911785f4f405d611c4b1e2d3
FediRouatbi <fedirouatbi@gmail.com>
2022-05-26 20:27:20 +0100

    'open close navbar'

--- src/components/Spend.js
+++ src/components/Spend.js
@@ -2,11 +2,16 @@ import React from "react";
 import { GiWallet } from "react-icons/gi";
 import "./spend.scss";
 const Spend = ({ data }) => {
-  console.log(data);
+  const style = {
+    backgroundColor: data === "Total" && "#212529",
+    color: data === "Total" && "white",
+  };
+  const walletColor = { color: data === "Total" && "#94d82d" };
+  const backgroundWallet = { backgroundColor: data === "Total" && "#868e96" };
   return (
-    <div className="spend">
-      <div className="spend__icon">
-        <GiWallet />
+    <div className="spend" style={style}>
+      <div className="spend__icon" style={backgroundWallet}>
+        <GiWallet style={walletColor} />
       </div>
       <div className="spend__data">
         <div className="spend__data--text">{data} spending</div>
