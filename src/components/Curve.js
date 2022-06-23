c995e44b18a1888b80d013035aab22a5d6b8c3b2
FediRouatbi <fedirouatbi@gmail.com>
2022-06-23 12:08:27 +0100

    flexible chart

--- src/components/Curve.js
+++ src/components/Curve.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,8 +12,6 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
-
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -38,21 +37,30 @@ export const options = {
 };
 
 const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
 export const data = {
   labels,
   datasets: [
     {
       fill: true,
       label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      data: [10, 5, 122, 990, 223, 500, 450],
       borderColor: "rgb(53, 162, 235)",
       backgroundColor: "rgba(53, 162, 235, 0.5)",
     },
   ],
 };
 const Curve = () => {
-  return <Line options={options} data={data} />;
+  return (
+    <div className=" p-2 rounded-lg border-gray-400 border h-64">
+      <div className="h-60 mx-auto">
+        <Line
+          options={(options, { maintainAspectRatio: false })}
+          data={data}
+          height={150}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Curve;
